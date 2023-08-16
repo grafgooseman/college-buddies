881efbace182fa6c080d3df2ad6f30e89ba3184b
Artem Gusev <artemgusev2100@gmail.com>
2023-08-16 06:43:09 -0400

    Button is a separate componenet, old data removed

--- app/page.tsx
+++ app/page.tsx
@@ -2,71 +2,75 @@ import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Image from "next/image";
-import Button from "@mui/material/Button";
+
+import TryNow from "@/components/TryNow";
+
 // import useMediaQuery from '@mui/material/useMediaQuery';
 // import { useTheme } from '@mui/material/styles'; Can't be used on a server side componenet
 
 export default function Home() {
-  // const theme = useTheme();
-  // const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
-  // const isSmallScreen = useMediaQuery('(max-width:600px)');
+    // const theme = useTheme();
+    // const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+    // const isSmallScreen = useMediaQuery('(max-width:600px)');
 
-  // const h3Variant = isSmallScreen ? 'h5' : 'h3';
-  // const subtitleVariant = isSmallScreen ? 'body1' : 'subtitle1';
+    // const h3Variant = isSmallScreen ? 'h5' : 'h3';
+    // const subtitleVariant = isSmallScreen ? 'body1' : 'subtitle1';
 
+    return (
+        <Container>
+            <Box className="mt-5 md:flex">
+                <Box className="relative">
+                    <Image
+                        //Photo by Philippe Bout on Unsplash
+                        className="cut-corners-mobile md:cut-corners w-full h-full object-cover"
+                        src={"/dude-in-auditorium.jpg"}
+                        width={700}
+                        height={700}
+                        alt="Person sitting alone in auditorium"
+                    />
+                    {/* <Box className="absolute inset-0 bg-gradient-to-bl from-transparent to-white"></Box> */}
+                    <Box className="absolute inset-0 md:hidden flex flex-col justify-center sm:mb-16">
+                        <Typography
+                            variant="h3"
+                            className="w-8/12 text-2xl sm:text-3xl"
+                        >
+                            Find{" "}
+                            <span className="text-accent">meaningfull</span>{" "}
+                            connections
+                        </Typography>
+                        <Typography
+                            variant="subtitle1"
+                            className="w-7/12 sm:text-base text-xs"
+                        >
+                            Get to know people studying in the{" "}
+                            <span className="text-accent">same college</span>
+                        </Typography>
+                        <Box className="pt-3 w-1/2">
+                            <TryNow
+                                size="medium"
+                                className="bg-accent rounded-2xl mt-2 hover:bg-hoveraccent"
+                            />
+                        </Box>
+                    </Box>
+                </Box>
 
-  return (
-    <Container>
-      <Box className="mt-5 md:flex">
-        <Box className="relative">
-          <Image
-            //Photo by Philippe Bout on Unsplash
-            className="cut-corners-mobile md:cut-corners w-full h-full object-cover"
-            src={"/dude-in-auditorium.jpg"}
-            width={700}
-            height={700}
-            alt="Person sitting alone in auditorium"
-          />
-          {/* <Box className="absolute inset-0 bg-gradient-to-bl from-transparent to-white"></Box> */}
-          <Box className="absolute inset-0 md:hidden flex flex-col justify-center sm:mb-16">
-            <Typography variant="h3" className="w-8/12 text-2xl sm:text-3xl">
-              Find <span className="text-accent">meaningfull</span> connections
-            </Typography>
-            <Typography variant="subtitle1" className="w-7/12 sm:text-base text-xs">
-              Get to know people studying in the{" "}
-              <span className="text-accent">same college</span>
-            </Typography>
-            <Box className="pt-3 w-1/2">
-              <Button
-                variant="contained"
-                size="medium"
-                className="bg-accent rounded-2xl mt-2 hover:bg-hoveraccent"
-              >
-                TRY NOW
-              </Button>
+                <Box className="hidden md:flex flex-col justify-center align-middle md:pl-2 md:mb-14 md:space-y-2">
+                    <Typography variant="h3">
+                        Find <span className="text-accent">meaningfull</span>{" "}
+                        connections
+                    </Typography>
+                    <Typography variant="subtitle1">
+                        Get to know people studying in the{" "}
+                        <span className="text-accent">same college</span>
+                    </Typography>
+                    <Box className="pt-3">
+                        <TryNow
+                            size="large"
+                            className="bg-accent rounded-2xl md:mt-2 hover:bg-hoveraccent"
+                        />
+                    </Box>
+                </Box>
             </Box>
-          </Box>
-        </Box>
-
-        <Box className="hidden md:flex flex-col justify-center align-middle md:pl-2 md:mb-14 md:space-y-2">
-          <Typography variant="h3">
-            Find <span className="text-accent">meaningfull</span> connections
-          </Typography>
-          <Typography variant="subtitle1">
-            Get to know people studying in the{" "}
-            <span className="text-accent">same college</span>
-          </Typography>
-          <Box className="pt-3">
-            <Button
-              variant="contained"
-              size="large"
-              className="bg-accent rounded-2xl md:mt-2 hover:bg-hoveraccent"
-            >
-              TRY NOW
-            </Button>
-          </Box>
-        </Box>
-      </Box>
-    </Container>
-  );
+        </Container>
+    );
 }
--- components/TryNow.tsx
+++ components/TryNow.tsx
@@ -1,2 +1,22 @@
+"use client";
 
-  
+import Button from "@mui/material/Button";
+
+export default function TryNow( {size, className}: {size: "small" | "medium" | "large"; className?: string} ) {
+
+    const handleClick = () => {
+        console.log("Try Now");
+    }
+
+
+    return (
+        <Button
+            onClick={handleClick}
+            variant="contained"
+            size={size}
+            className={className}
+        >
+            TRY NOW
+        </Button>
+    );
+}
